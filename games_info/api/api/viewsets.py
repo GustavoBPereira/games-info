2303f13ddce1ed6d57a8947daea62c7586fee466
Gustavo Brito <britopereiragustavo@gmail.com>
2020-03-22 21:42:05 -0300

    Updating a game if this game exists

--- games_info/api/api/viewsets.py
+++ games_info/api/api/viewsets.py
@@ -22,11 +22,20 @@ class GameViewSet(viewsets.ModelViewSet):
         crawler = GameCrawler(searched_game)
         game_data_steamdb = crawler.get_data_from_steamdb()
         crawler.close()
+
         game_name = game_data_steamdb['real_name']
         current_price = game_data_steamdb['current_price']
         best_price = game_data_steamdb['best_price']
 
-        game = Game.objects.create(searched_game=searched_game, game_name=game_name,
-                                   current_price=current_price, best_price=best_price)
+        try:
+            game = Game.objects.get(game_name=game_name)
+            game.searched_game = searched_game
+            game.game_name = game_name
+            game.current_price = current_price
+            game.best_price = best_price
+            game.save()
+        except Game.DoesNotExist:
+            game = Game.objects.create(searched_game=searched_game, game_name=game_name,
+                                       current_price=current_price, best_price=best_price)
         serializer = GameSerializer(game)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
