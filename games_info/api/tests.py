46bd2c415ff86b9a1de9ca9b36a06367ddf62aa4
Gustavo Brito <britopereiragustavo@gmail.com>
2020-03-28 16:02:14 -0300

    Adding Tests

--- games_info/api/tests.py
+++ games_info/api/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from games_info.api.models import Game
+
+
+class HomeTest(TestCase):
+
+    # Removing game that will be tested
+    try:
+        game = Game.objects.get(game_name="Hellblade: Senua's Sacrifice")
+        game.delete()
+    except Game.DoesNotExist:
+        pass
+
+    c = Client()
+    json_response = c.post('/games-api/', {'searched_game': 'hellblade'})
+
+    def test_response_with_currect_name(self):
+        self.assertEqual(self.json_response.json()['game_name'], "Hellblade: Senua's Sacrifice")
+
+    def test_status_code_new_game_searched(self):
+        self.assertEqual(self.json_response.status_code, 201)
