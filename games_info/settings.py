cb51446bfa57eba7dcc2e0e850a2e8d4624be357
Gustavo Brito <britopereiragustavo@gmail.com>
2020-04-18 23:45:18 -0300

    heroku things

--- games_info/settings.py
+++ games_info/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://games-info.herokuapp.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
