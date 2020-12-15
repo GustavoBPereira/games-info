db48c62afe2e91d6e4861426383d45f57f57d49c
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-14 23:39:55 -0300

    creating choices with accepted_currency from crawler

--- games_info/front_end/forms.py
+++ games_info/front_end/forms.py
@@ -1,20 +1,10 @@
 from django import forms
 
+from games_info.crawler import accepted_currency
+
 
 class SearchGameForm(forms.Form):
-    CHOICES = choices = (
-        ('us', 'U.S.Dollar'), ('eu', 'Euro'), ('ar', 'Argentine Peso'), ('au', 'Australian Dollar'),
-        ('br', 'Brazilian Real'), ('uk', 'British Pound'), ('ca', 'Canadian Dollar'), ('cl', 'Chilean Peso'),
-        ('cn', 'Chinese Yuan'), ('az', 'CIS - U.S.Dollar'), ('co', 'Colombian Peso'), ('cr', 'Costa Rican Colon'),
-        ('hk', 'Hong Kong Dollar'), ('in', 'Indian Rupee'), ('id', 'Indonesian Rupiah'), ('il', 'Israeli New Shekel'),
-        ('jp', 'Japanese Yen'), ('kz', 'Kazakhstani Tenge'), ('kw', 'Kuwaiti Dinar'), ('my', 'Malaysian Ringgit'),
-        ('mx', 'Mexican Peso'), ('nz', 'New Zealand Dollar'), ('no', 'Norwegian Krone'), ('pe', 'Peruvian Sol'),
-        ('ph', 'Philippine Peso'), ('pl', 'Polish Zloty'), ('qa', 'Qatari Riyal'), ('ru', 'Russian Ruble'),
-        ('sa', 'Saudi Riyal'), ('sg', 'Singapore Dollar'), ('za', 'South African Rand'), ('pk', 'South Asia - USD'),
-        ('kr', 'South Korean Won'), ('ch', 'Swiss Franc'), ('tw', 'Taiwan Dollar'), ('th', 'Thai Baht'),
-        ('tr', 'Turkish Lira'), ('ae', 'U.A.E.Dirham'), ('ua', 'Ukrainian Hryvnia'), ('uy', 'Uruguayan Peso'),
-        ('vn', 'Vietnamese Dong')
-    )
+    CHOICES = [(currency, accepted_currency[currency]['name']) for currency in accepted_currency]
 
     game_search = forms.CharField(max_length=64)
     currency = forms.ChoiceField(choices=CHOICES)
