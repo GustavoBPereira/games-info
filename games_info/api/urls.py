bfbe43c9c1706c63518357904b078c5c0191bc7d
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-15 22:46:25 -0300

    app_name

--- games_info/api/urls.py
+++ games_info/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from games_info.api.views import GameInfo, app_ids
 
+app_name = 'api'
 urlpatterns = [
-    path('game/', GameInfo.as_view(), name='api'),
+    path('game/', GameInfo.as_view(), name='game'),
     path('app_ids/', app_ids, name='api_app_ids'),
 ]
