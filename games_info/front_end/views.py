0d3a75a94a7d55e064eb7852f89740649648a9b7
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-27 11:54:09 -0300

    removing imports.

--- games_info/front_end/views.py
+++ games_info/front_end/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.urls import reverse
-import requests
 
 from games_info.front_end.forms import SearchGameForm
-
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.functional import SimpleLazyObject
 
 
 class Index(View):
