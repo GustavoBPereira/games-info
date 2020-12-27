c568c42473e05faf84132f46b2ed24968fab9d1b
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-27 11:53:45 -0300

    removing commented code.

--- games_info/front_end/views.py
+++ games_info/front_end/views.py
@@ -15,13 +15,3 @@ class Index(View):
     def get(self, *args, **kwargs):
         form = SearchGameForm()
         return render(self.request, 'index.html', {'form': form})
-
-    # def post(self, *args, **kwargs):
-    #     form = SearchGameForm(self.request.POST)
-    #     game = form['game_search'].value()
-    #     currency = form['currency'].value()
-    #     # current_site = SimpleLazyObject(lambda: get_current_site(self.request))
-    #     # url = 'http://' + str(current_site) + reverse('api')
-    #     url = reverse('api:game')
-    #     # game_data = requests.post(url=url, data={'searched_game': game, 'currency': currency})
-    #     return render(self.request, 'index.html', {'game': game_data.json(), 'form': form})
