2021ea5c380a362c1bc50bbf20391b7edd1c5957
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-31 09:49:34 -0300

    rendering price

---
+++ games_info/front_end/templatetags/custom_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+def render_price(value):
+    if value['is_free']:
+        return 'Free to Play'
+    else:
+        if int(value['discount_percent']) == 0:
+            return value['final_formatted']
+        else:
+            text = f'<span class="percent_discount">{value["discount_percent"]}% </span>' \
+                   f'<span class="risked_initial">{value["initial_formatted"]} </span>' \
+                   f'<span class="final_formatted">{value["final_formatted"]}</span>'
+            return mark_safe(text)
+
+
+register.filter('render_price', render_price)
